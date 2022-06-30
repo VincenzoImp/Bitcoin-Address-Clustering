377a243c6b28a33efff7571cce449711df57c5b0
VincenzoImp <vincenzoimperati99@gmail.com>
2022-06-30 19:00:45 +0200

    update

--- app/app.py
+++ app/app.py
@@ -15,81 +15,26 @@ from urllib.error import HTTPError
 from pyvis.network import Network
 import base64
 from PIL import Image
-
-
-def generate_pyvis_graph(v_df, e_df, a_df, cluster_id_selected):
-    addr_clust = {row.address : row.cluster_id for row in a_df.rdd.collect()}
-    net = Network(
-                height='100%',
-                width='80%',
-                directed=True
-                )
-    net.show_buttons(filter_=["physics"])
-
-    for node in v_df.rdd.collect():
-        if node.note == 'tx':
-            title = 'tx_hash: {}\n'.format(node.tx_hash) + \
-                'block_height: {}\n'.format(node.block_height) + \
-                'n_input: {}\n'.format(node.n_input) + \
-                'n_output: {}\n'.format(node.n_output) + \
-                'amount_input: {} BTC\n'.format(node.amount_input/100000000) + \
-                'amount_output: {} BTC\n'.format(node.amount_output/100000000) + \
-                'fee: {} BTC'.format(node.fee/100000000)
-            net.add_node(n_id=str(node.id), shape='dot', title=title)
-        elif node.note == 'UTXO':
-            title = '{}\n'.format(node.note) + \
-                'amount: {} BTC'.format(node.amount_input/100000000)
-            net.add_node(n_id=str(node.id), shape='square', title=title)
-        elif node.note == 'coinbase':
-            title = '{}\n'.format(node.note) + \
-                'amount: {} BTC'.format(node.amount_output/100000000)
-            net.add_node(n_id=str(node.id), shape='square', title=title)
-        elif node.note == 'unknown_tx':
-            title = 'unknown_tx\n' + 'tx_index: {}'.format(node.id)
-            net.add_node(n_id=str(node.id), shape='square', title=title)
-    for edge in e_df.rdd.collect():
-        if edge.address[:4] != 'UTXO' and edge.address[:4] != 'coin':
-            title = 'address: {}\n'.format(edge.address) + \
-                    'value: {} BTC\n'.format(edge.value/100000000) + \
-                    'cluster_id : {}'.format(addr_clust[edge.address])
-            if addr_clust[edge.address] == cluster_id_selected:
-                net.add_edge(source=str(edge.src_id), to=str(edge.dst_id), title=title, value=edge.value/100000000, color='red')
-            else:
-                net.add_edge(source=str(edge.src_id), to=str(edge.dst_id), title=title, value=edge.value/100000000)
-        else:
-            title = 'address: {}\n'.format(edge.address) + \
-                    'value: {} BTC\n'.format(edge.value/100000000)
-            net.add_edge(source=str(edge.src_id), to=str(edge.dst_id), title=title, value=edge.value/100000000)
-    return net
-
-
-def render_svg(svg_file):
-    with open(svg_file, "r") as f:
-        lines = f.readlines()
-        svg = "".join(lines)
-        b64 = base64.b64encode(svg.encode("utf-8")).decode("utf-8")
-        html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
-        html = '<p style="text-align:center;">' + html + '</p>'
-        return html
+import pandas as pd
 
 
 
 
+img = Image.open('Bitcoin.png')
+st.set_page_config(
+    page_title="Bitcoin Address Clustering",
+    layout="wide",
+    page_icon=img
+)
 
 
-
-def main(start_block, end_block):
-
-    print('executing main')
-
-    DIR = './'
-
-    spark = SparkSession.builder.getOrCreate()
-    d_path = os.path.join(DIR, 'blocks-{}-{}'.format(start_block, end_block))
+@st.cache
+def load_df(start_block, end_block, d_path):
+    
     e_path = os.path.join(d_path, 'edges-{}-{}'.format(start_block, end_block))
     v_path = os.path.join(d_path, 'vertices-{}-{}'.format(start_block, end_block))
     a_path = os.path.join(d_path, 'addresses-{}-{}'.format(start_block, end_block))
-
+    spark = SparkSession.builder.getOrCreate()
     e_df = spark.read.load(e_path,
                             format="csv",
                             sep=",",
@@ -120,14 +65,84 @@ def main(start_block, end_block):
                             inferSchema="true",
                             header="true"
                             )
+    return e_df.toPandas(), v_df.toPandas(), a_df.toPandas()
 
 
-    img = Image.open('Bitcoin.png')
-    st.set_page_config(
-        page_title="Bitcoin Address Clustering",
-        layout="wide",
-        page_icon=img
-    )
+
+
+def generate_pyvis_graph(v_df, e_df, a_df, cluster_id_selected):
+
+    net = Network(
+                height='100%',
+                width='80%',
+                directed=True
+                )
+    net.show_buttons(filter_=["physics"])
+
+    for _, node in v_df.iterrows():
+        if node.note == 'tx':
+            title = 'tx_hash: {}\n'.format(node.tx_hash) + \
+                'block_height: {}\n'.format(node.block_height) + \
+                'n_input: {}\n'.format(node.n_input) + \
+                'n_output: {}\n'.format(node.n_output) + \
+                'amount_input: {} BTC\n'.format(node.amount_input/100000000) + \
+                'amount_output: {} BTC\n'.format(node.amount_output/100000000) + \
+                'fee: {} BTC'.format(node.fee/100000000)
+            net.add_node(n_id=str(node.id), shape='dot', title=title)
+        elif node.note == 'UTXO':
+            title = '{}\n'.format(node.note) + \
+                'amount: {} BTC'.format(node.amount_input/100000000)
+            net.add_node(n_id=str(node.id), shape='square', title=title)
+        elif node.note == 'coinbase':
+            title = '{}\n'.format(node.note) + \
+                'amount: {} BTC'.format(node.amount_output/100000000)
+            net.add_node(n_id=str(node.id), shape='square', title=title)
+        elif node.note == 'unknown_tx':
+            title = 'unknown_tx\n' + 'tx_index: {}'.format(node.id)
+            net.add_node(n_id=str(node.id), shape='square', title=title)
+
+    for _, edge in e_df.iterrows():
+        try:
+            if edge.address[:4] != 'UTXO' and edge.address[:4] != 'coin':
+                cluster_id = a_df[a_df['address'] == edge.address].cluster_id.iloc[0]
+                title = 'address: {}\n'.format(edge.address) + \
+                        'value: {} BTC\n'.format(edge.value/100000000) + \
+                        'cluster_id : {}'.format(cluster_id)
+                if cluster_id == cluster_id_selected:
+                    net.add_edge(source=str(edge.src_id), to=str(edge.dst_id), title=title, value=edge.value/100000000, color='red')
+                else:
+                    net.add_edge(source=str(edge.src_id), to=str(edge.dst_id), title=title, value=edge.value/100000000)
+            else:
+                title = 'address: {}\n'.format(edge.address) + \
+                        'value: {} BTC\n'.format(edge.value/100000000)
+                net.add_edge(source=str(edge.src_id), to=str(edge.dst_id), title=title, value=edge.value/100000000)
+        except:
+            pass
+    return net
+
+
+def render_svg(svg_file):
+    with open(svg_file, "r") as f:
+        lines = f.readlines()
+        svg = "".join(lines)
+        b64 = base64.b64encode(svg.encode("utf-8")).decode("utf-8")
+        html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+        html = '<p style="text-align:center;">' + html + '</p>'
+        return html
+
+
+
+
+
+
+
+def main(start_block, end_block):
+
+    print('executing main')
+    DIR = './'
+    d_path = os.path.join(DIR, 'blocks-{}-{}'.format(start_block, end_block))
+
+    e_df, v_df, a_df = load_df(start_block, end_block, d_path)
 
     st.title('Bitcoin Address Clustering\n')
     with st.container():
@@ -159,30 +174,27 @@ def main(start_block, end_block):
 
     if submit_button:
         with st.spinner("Loading..."):
-
-            a_df.createOrReplaceTempView('ADDRESSES')
-            if spark.sql("select * from ADDRESSES where address = '"+address+"'").rdd.isEmpty():
+            row = a_df[a_df.address == address]
+            if row.empty:
                 print('invalid address')
                 st.error('Invalid address')
             else:
-                a_df.createOrReplaceTempView('ADDRESSES')
-                cluster_id = spark.sql("select cluster_id from ADDRESSES where address = '"+address+"'").head().cluster_id
-                a_df.createOrReplaceTempView('ADDRESSES')
-                ClusterList = spark.sql("select address from ADDRESSES where cluster_id = " + str(cluster_id)).withColumnRenamed('address', 'address_clustered')
+                # get cluster id of address entered
+                cluster_id = row.cluster_id.iloc[0]
+                # select all address in cluster id
+                addrs_in_cluster_df = pd.DataFrame({'address' : a_df[a_df.cluster_id == cluster_id].address})
 
                 print('address {} is in cluster {}'.format(address, cluster_id))
                 print('generating cluster graphs')
 
-                new_e_df = e_df.join(ClusterList, e_df.address == ClusterList.address_clustered, 'inner').drop('address_clustered')
-                tmp_df = new_e_df.select(new_e_df.src_id).union(new_e_df.select(new_e_df.dst_id)).withColumnRenamed('src_id', 'tmp_id').distinct()
-                new_v_df = v_df.join(tmp_df, v_df.id == tmp_df.tmp_id, 'inner').drop('tmp_id')
-                tmp_df = new_v_df.select('id')
-                new_e_df = e_df.join(tmp_df, tmp_df.id == e_df.src_id, 'inner').union(e_df.join(tmp_df, tmp_df.id == e_df.dst_id, 'inner')).drop('id').distinct()
-                tmp_df = new_e_df.select(new_e_df.src_id).union(new_e_df.select(new_e_df.dst_id)).distinct().withColumnRenamed('src_id', 'tmp_id')
-                new_v_df = v_df.join(tmp_df, v_df.id == tmp_df.tmp_id).drop('tmp_id')
+                new_e_df = e_df[e_df.address.isin(addrs_in_cluster_df.address)]
+                new_v_df = v_df[v_df.id.isin(new_e_df.src_id) | v_df.id.isin(new_e_df.dst_id)]
+                new_e_df = e_df[e_df.src_id.isin(new_v_df.id) | e_df.dst_id.isin(new_v_df.id)]
+                new_v_df = v_df[v_df.id.isin(new_e_df.src_id) | v_df.id.isin(new_e_df.dst_id)]
 
-                n_v = new_v_df.count()
-                n_e = new_e_df.count()
+                n_v = new_v_df.shape[0]
+                n_e = new_e_df.shape[0]
+                print(n_v, n_e)
 
                 if n_v + n_e > 1400:
                     st.error('Too large graph to plot: {} nodes, {} edges'.format(n_v, n_e))
@@ -197,17 +209,18 @@ def main(start_block, end_block):
                     print('cluster graphs generated')
                     print('display cluster')
                     st.success("Done!")
+
                     HtmlFile = open(os.path.join(d_path, 'cluster_graph-{}-{}.html'.format(start_block, end_block)), 'r', encoding='utf-8')
                     components.html(HtmlFile.read(), height=800, scrolling=True)
 
                     print('cluster displayed')
-
+                    
                     st.subheader('Graph Nodes')
-                    st.dataframe(new_v_df.toPandas())
+                    st.dataframe(new_v_df.reset_index())
                     st.subheader('Graph Edges')
-                    st.dataframe(new_e_df.toPandas())
+                    st.dataframe(new_e_df.reset_index())
                     st.subheader('Clustered Addresses')
-                    st.dataframe(ClusterList.toPandas())
+                    st.dataframe(addrs_in_cluster_df.reset_index())
 
     print('end main')
     return
